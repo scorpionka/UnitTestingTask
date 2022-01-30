2477e796168c19a1e2cd7f1500ede3f87104f25a
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-01-30 23:24:30 +0300

    Add project files.

---
+++ Sudoku/Models/SudokuTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sudoku
+{
+    public class SudokuTable
+    {
+        public SudokuTable(int[,] sudokuTable)
+        {
+            Table = sudokuTable ?? throw new ArgumentNullException(nameof(sudokuTable));
+        }
+
+        public int[,] Table { get; private set; }
+    }
+}
