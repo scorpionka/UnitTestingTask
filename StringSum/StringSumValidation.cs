58eb7c7b7e84a218db1a8ec0d547f5be5e20071a
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-02-01 02:17:58 +0300

    feat(StringSum): add null check test fix(StringSum): fix validation error

--- StringSum/StringSumValidation.cs
+++ StringSum/StringSumValidation.cs
@@ -15,7 +15,7 @@ namespace StringSum
             if (string.IsNullOrWhiteSpace(num1))
                 throw new ArgumentException("num1 is empty string", nameof(num1));
 
-            if (!string.IsNullOrWhiteSpace(num2))
+            if (string.IsNullOrWhiteSpace(num2))
                 throw new ArgumentException("num2 is empty string", nameof(num2));
         }
     }
--- StringSumTests/StringSumCalculatorTests.cs
+++ StringSumTests/StringSumCalculatorTests.cs
@@ -12,9 +12,19 @@ namespace StringSumTests
         }
 
         [Test]
+        public void NumbersAreNull_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => StringSumCalculator.Sum(null, null));
+            Assert.Throws<ArgumentNullException>(() => StringSumCalculator.Sum("5", null));
+            Assert.Throws<ArgumentNullException>(() => StringSumCalculator.Sum(null, "3"));
+        }
+
+        [Test]
         public void NumbersAreEmptyStrings_ThrowsArgumentException()
         {
             Assert.Throws<ArgumentException>(() => StringSumCalculator.Sum(string.Empty, string.Empty));
+            Assert.Throws<ArgumentException>(() => StringSumCalculator.Sum("5", string.Empty));
+            Assert.Throws<ArgumentException>(() => StringSumCalculator.Sum(string.Empty, "3"));
         }
     }
 }
